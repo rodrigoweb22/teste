c69ab9d78dd2105c3ae2bfe866bee56a6e6551cc
rodrigoweb22 <164879453+rodrigoweb22@users.noreply.github.com>
2024-12-11 17:24:02 -0300

    Add files via upload

---
+++ js/menu2.php
@@ -0,0 +1,124 @@
+<header id="header" class="header fixed-top sticked" data-scrollto-offset="0">
+    <div class="container d-flex align-items-center justify-content-between container-custom">
+
+      <a href="/" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <img src="../assets/img/rensoftware-tecnologia-em-sistemas-logo.png" title="Rensoftware Tecnologia em Sistemas" alt="Rensoftware Tecnologia em sistemas" class="img-fluid logotopo">
+      </a>
+      <nav id="navbar" class="navbar">
+        <ul>
+        <a href="/" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
+        <img src="assets/img/rensoftware-tecnologia-em-sistemas-logo.png" title="Rensoftware Tecnologia em Sistemas" alt="Rensoftware Tecnologia em sistemas" class="img-fluid logo-mobile d-none">
+        </a>
+          <li class="dropdown megamenu">
+            <a href="#" style="color: white;" class="item-title">Soluções
+              <i class="bi bi-chevron-down dropdown-indicator" style="color: white;"></i></a>
+            <ul>
+              <li>
+                <div class="card" style="border-right:1px solid #ebebeb;">
+                  <div class="card-body">
+                    <h5 class="card-title">ERP'S</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      <a href="/sce-erp-desktop" class="sub-link" title="SCE - ERP Desktop"><i class="bi bi-caret-right-fill"></i> SCE - ERP Desktop </a><br>
+                      <a href="/easyren" class="sub-link easyren" title="EasyRen - ERP Nuvem "><i class="bi bi-caret-right-fill"></i> EasyRen - ERP Nuvem <span class="new">Novo</span></a><br></h6>
+                     
+                      <h5 class="card-title">E-COMMERCE</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">
+                        <a href="ecommerce" class="sub-link" title="Loja Virtual"> <i class="bi bi-caret-right-fill"></i> Loja Virtual</a><br>
+                        <a href="ecommerce" class="sub-link destaque" title="Catálogo Digital"><i class="bi bi-caret-right-fill"></i> Catálogo Digital</a><br>
+                        <a href="/dot-pedidos" class="sub-link" title="Dot Pedidos - Food"><i class="bi bi-caret-right-fill"></i> Dot Pedidos - Food <span class="new">Novo</span></a><br>
+                        </h6>
+                  </div>
+                </div>
+              </li>
+
+                <li style="margin-bottom: auto !important;">
+                  <div class="card" style="border-right:1px solid #ebebeb;">
+                    <div class="card-body">
+                      <h5 class="card-title">PDV's</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">
+                        <a href="/sce-erp-desktop#pdvDesktop" class="sub-link" title="SCE PDV - Desktop"><i class="bi bi-caret-right-fill"></i> SCE PDV - Desktop </a><br>
+                        <a href="/easyren#easyren-mobile" class="sub-link easyren" title="EasyRen PDV - Mobile"><i class="bi bi-caret-right-fill"></i> EasyRen PDV - Mobile</a><br>
+                      </h6>
+                        <h5 class="card-title">Vendas Externas</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                          <a href="/sce-erp-desktop#forcaDeVendas" class="sub-link" title="App Força de Vendas"><i class="bi bi-caret-right-fill"></i> App Força de Vendas</a><br>
+                          </h6>
+                    </div>
+                  </div>
+                </li>
+
+                <li style="margin-bottom: auto !important;">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Mais Produtos</h5>
+                      <h6 class="card-subtitle mb-2 text-muted">
+                        <a href="/md-e-busca-nf-e" class="sub-link"><i class="bi bi-caret-right-fill"></i> MD-e - Busca NF-e</a><br>
+                        <a href="scan-coletor" class="sub-link destaque" title="App Scan coletor"><i class="bi bi-caret-right-fill"></i> App Scan coletor</a><br>
+                        <a href="sce-focus" class="sub-link" title="App SCE Focus"><i class="bi bi-caret-right-fill"></i> App SCE Focus</a><br>
+                      </h6>
+                    </div>
+                  </div>
+                </li>
+            </ul>
+          </li>
+
+          <li class="dropdown megamenu menuheader">
+            <a href="#" style="color: white;" class="item-title">Segmentos<i class="bi bi-chevron-down dropdown-indicator" style="color: white;"></i></a>
+             <ul>
+                   <li class="estilo-mobile"><a href="segmentos" class="btn btn-link link-1" style="justify-content: center; color:#00aeff; text-decoration: none;"> <img src="assets/img/icone/supermercado.png" class="icones-desk"> <img src="assets/img/icone/s-bg.png" class="d-none icones-mobile"> <b>Supermercados</b></a></li>
+                   <li class="estilo-mobile"><a href="segmentos" class="btn btn-link link-2" style="justify-content: center; color:#ff9700; text-decoration: none; border-left: 1px solid #e5e5e5;"> <img src="assets/img/icone/construcao.png" class="icones-desk"> <img src="assets/img/icone/c-bg.png" class="d-none icones-mobile"> <b>Construção</b></a></li>
+                   <li class="estilo-mobile"><a href="segmentos" class="btn btn-link link-3" style="justify-content: center; color:#212869; text-decoration: none; border-right: 1px solid #e5e5e5; border-left: 1px solid #e5e5e5;"><img src="assets/img/icone/atacarejos.png" class="icones-desk"> <img src="assets/img/icone/a-bg.png" class="d-none icones-mobile"> <b>Atacarejos</b></a></li>
+                   <li class="estilo-mobile"><a href="segmentos" class="btn btn-link link-4" style="justify-content: center; color:#3350b9; text-decoration: none; border-right: 1px solid #e5e5e5;"> <img src="assets/img/icone/farmacia.png" class="icones-desk"> <img src="assets/img/icone/f-bg.png" class="d-none icones-mobile"><b>Farmácia</b></a></li>
+                   <li class="estilo-mobile"><a href="segmentos" class="btn btn-link texto-negrito link-5" style="justify-content: center; color:#ae0909; text-decoration: none;"> <img src="assets/img/icone/delivery.png" class="icones-desk">  <img src="assets/img/icone/d-bg.png" class="d-none icones-mobile"> Delivery</a></li>
+                   <li class="estilo-mobile"><a href="segmentos" class="btn btn-link texto-negrito" style="justify-content: center; color:#000000; text-decoration: none; border-left: 1px solid #e5e5e5;"> + Segmentos</a></li>
+             </ul>
+          </li> 
+
+          <li class="dropdown item-title"><a href="#" style="color: white;">Institucional<i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li class="institucional"><a href="/institucional">A Empresa</a></li>
+              <li class="institucional"><a href="/representantes">Representantes</a></li>
+              <li class="institucional"><a href="/trabalhe-conosco">Trabalhe Conosco</a></li>
+              <li class="institucional"><a href="/blog">Blog</a></li>
+            </ul>
+          </li>
+        
+          <li class="dropdown item-title"><a href="#" style="color: white;">Portais<i class="bi bi-chevron-down dropdown-indicator"></i></a>
+            <ul>
+              <li class="institucional"><a href="https://ambiente.rensoftware.com.br/login/index/" target="_blank">Cliente</a></li>
+              <li class="institucional"><a href="https://ambiente.rensoftware.com.br/login/index/" target="_blank">Representantes</a></li>
+              <li class="institucional"><a href="https://ambiente.rensoftware.com.br/login/index/" target="_blank">Parceiros</a></li>
+              <li class="institucional"><a href="https://ambiente.rensoftware.com.br/login/index/" target="_blank">Contador</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto item-title" href="suporte-tecnico-rensoftware" style="color: white;">Suporte</a></li>
+          <li><a class="nav-link scrollto" href="contato" style="color: white;">Contato</a></li>
+
+          <div>
+          <a href="tel:0800 730 1222" data-ga="true" data-ga-action="Click to Call - Vendas" data-ga-label="Header" style="color: #fff;
+          font-size: 17px;
+          margin-right: 10px;
+          margin-top: 10px;
+          padding: 10px;
+          font-weight: 700;
+          line-height: 0;">0800 730 1222</a>
+          <span style="    color: #c0bebe;
+          font-family: Lato,sans-serif;
+          font-size: 9px;
+          font-weight: 400;
+          text-transform: uppercase;
+          margin-left: 24px;
+          letter-spacing: -.5px;
+          text-align: center;">CENTRAL DE VENDAS</span>
+        </div>
+
+          <li>
+
+            <button type="button" class="btn btn-warning btn-sm show btn-mobile" style="font-weight: 600;" aria-haspopup="true">Agendar demonstração</button></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle d-none" style="color: aliceblue;"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>
